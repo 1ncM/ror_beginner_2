e11a3e7efa79177aed6c8588308bd8720e2f15d0
Alexandr <microposts@gmail.com>
2015-12-11 00:37:42 +0300

    Added area/right_triangle

---
+++ part_1/area_of_a_triangle.rb
@@ -0,0 +1,6 @@
+puts "Enter the base of the triangle"
+base = gets.chomp
+puts "Enter the height of the triangle"
+height = gets.chomp
+area_of_a_triangle = base.to_i/2*height.to_i
+puts "Area of a triangle is equal to #{area_of_a_triangle}"
