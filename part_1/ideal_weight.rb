f4e80a3377189e7b4439ac548d23612fcbc2ee06
Alexandr <microposts@gmail.com>
2015-12-13 14:02:16 +0300

    Fix ideal_weight.rb

--- part_1/ideal_weight.rb
+++ part_1/ideal_weight.rb
@@ -1,8 +1,8 @@
 puts "Enter your name"
-name = gets.chomp
+name = gets.chomp.to_f
 puts "Enter your height"
-height = gets.chomp
-ideal_weight = height.to_i - 110
+height = gets.chomp.to_f
+ideal_weight = height - 110
 if ideal_weight < 0
   puts "Dear #{name}, your weight has optimum !"
 else
