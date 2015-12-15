bfc8d607723bb002875368ea232cfd9ad0e4829f
1ncM <microposts@gmail.com>
2015-12-16 00:29:11 +0300

    Update route.rb

--- part_3/route.rb
+++ part_3/route.rb
@@ -3,15 +3,12 @@ class Route
   def initialize
     @stations = []
   end
-
   def add_station(station)
     stations << station
   end
-
   def delete_station(station)
     stations.delete(station)
   end
-
   def to_s
   	"Маршрут: #{stations}"
   end
