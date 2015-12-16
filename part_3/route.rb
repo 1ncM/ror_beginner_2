6d4b04fe7be7ebdddcd6601f6c43164feed4c72e
Alexandr <microposts@gmail.com>
2015-12-16 23:40:40 +0300

    fix #add_station method

--- part_3/route.rb
+++ part_3/route.rb
@@ -1,10 +1,10 @@
 class Route
   attr_reader :stations
-  def initialize
-    @stations = []
+  def initialize(first_station,last_station)
+    @stations = [first_station,last_station]
   end
   def add_station(station)
-    stations << station
+    stations.insert(-2, station)
   end
   def delete_station(station)
     stations.delete(station)
@@ -13,3 +13,4 @@ class Route
   	"Маршрут: #{stations}"
   end
 end
+
