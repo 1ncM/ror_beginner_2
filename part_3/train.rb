4e07f511e23908088d0bbb13972f116a2c8b9411
Alexandr <microposts@gmail.com>
2015-12-17 22:01:41 +0300

    fix #go_station and delete Array @route

--- part_3/train.rb
+++ part_3/train.rb
@@ -6,7 +6,7 @@ class Train
     @type = type
     @number = number
     @speed = 0
-    @route = []
+    # @route = []
   end
   def add_wagon
     self.number += 1 if speed.zero?
@@ -14,22 +14,25 @@ class Train
   def delete_wagon
     self.number -= 1 if speed.zero? && number>0
   end
-  def get_route(route)
-    self.route << route
+  def set_route(route)
+    self.route = route
     self.current_station = route.stations.first
   end
   def go_station(station)
-    route.first.stations.each {|i| self.current_station = i if i.name == station} 
+    # route.stations.each {|i| self.current_station = i if i.name == station} 
+    if route.stations.include?(station) && (self.next_station || self.prev_station == station)
+      self.current_station = station 
+    end
   end
   def next_station
-    route.first.stations[route.first.stations.index(current_station) + 1]
+    route.stations[route.stations.index(current_station) + 1]
   end
   def prev_station
-    route.first.stations[route.first.stations.index(current_station) - 1]
+    route.stations[route.stations.index(current_station) - 1]
   end
   def to_s
-    "Имя: #{name}, Тип: #{type}, Вагонов: #{number}, Маршрут: #{route}"
+    "Имя: #{name}, Тип: #{type}, Вагонов: #{number}"
   end
 private
-  attr_writer :number, :current_station
+  attr_writer :number, :current_station, :route
 end
