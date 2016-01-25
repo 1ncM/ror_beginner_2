5b75a7b269292e75ff31742511da4e00b95da44f
Alexandr <microposts@gmail.com>
2016-01-25 22:57:03 +0300

    Added 10th lesson

---
+++ ror/tutu/app/views/trains/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @train, :id, :train_number, :created_at, :updated_at
---
+++ ror/tutu/db/migrate/20160125194913_create_trains.rb
@@ -0,0 +1,9 @@
+class CreateTrains < ActiveRecord::Migration
+  def change
+    create_table :trains do |t|
+      t.integer :train_number
+
+      t.timestamps null: false
+    end
+  end
+end
