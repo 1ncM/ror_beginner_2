25c03134f2ddbc8cca1b1b24261b5395344c274f
Alexandr <microposts@gmail.com>
2016-02-24 23:37:10 +0300

    fix style

--- ror/tutu/app/models/railway_station.rb
+++ ror/tutu/app/models/railway_station.rb
@@ -1,4 +1,5 @@
 class RailwayStation < ActiveRecord::Base
   has_many :railway_stations_routes
   has_many :railway_stations, through: :railway_stations_routes
+  validates :title, presence: true
 end
