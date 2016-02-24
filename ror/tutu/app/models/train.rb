a3e0d2a6d9d37501939c545dff50f5d87fde2e2d
Alexandr <microposts@gmail.com>
2016-02-24 21:44:06 +0300

    add car control

--- ror/tutu/app/models/train.rb
+++ ror/tutu/app/models/train.rb
@@ -3,4 +3,8 @@ class Train < ActiveRecord::Base
   has_many :tickets
   has_many :vagons
   validates :train_number, presence: true, uniqueness: true
+  
+  def vagon_type(type)
+    vagons.where(vagon_type: type)
+  end
 end
