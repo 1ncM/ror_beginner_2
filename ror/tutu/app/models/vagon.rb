28eaa8988d585861ec61b5f9bc3e7f04e97633fc
Alexandr <microposts@gmail.com>
2016-02-27 22:34:25 +0300

    fix set_number

--- ror/tutu/app/models/vagon.rb
+++ ror/tutu/app/models/vagon.rb
@@ -1,10 +1,12 @@
 class Vagon < ActiveRecord::Base
   belongs_to :train
   before_validation :set_number
-
   private
 
   def set_number
-  	self.number = self.class.all.count + 1
+  	self.number = self.class.all.count.to_i + 1
+  end
+
+  def valid_number
   end
 end
