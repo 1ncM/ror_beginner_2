8b78d2f00de6b8fc5d905d27c09a0fb9fbfd2db7
Alexandr <microposts@gmail.com>
2016-03-02 23:25:47 +0300

    Added method update_position to model RailwayStation

--- ror/app_test/app/models/railway_station.rb
+++ ror/app_test/app/models/railway_station.rb
@@ -1,4 +1,9 @@
 class RailwayStation < ActiveRecord::Base
   has_many :routes_stations
   has_many :routes, through: :routes_stations
+
+  def update_position(route, position)
+    station_route = routes_stations.where(route: route).first
+    station_route.update(position: position) if station_route
+  end
 end
