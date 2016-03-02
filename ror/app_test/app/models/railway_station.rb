52516b91fd6a7ad90d217e9ca0e67833c83b224a
Alexandr <microposts@gmail.com>
2016-03-03 00:02:34 +0300

    fix

--- ror/app_test/app/models/railway_station.rb
+++ ror/app_test/app/models/railway_station.rb
@@ -3,11 +3,18 @@ class RailwayStation < ActiveRecord::Base
   has_many :routes, through: :routes_stations
 
   def update_position(route, position)
-    station_route = routes_stations.where(route: route).first
+    station_route = station_route(route)
     station_route.update(position: position) if station_route
   end
 
   def position_in(route)
-    station_route.try(:position)
+    station_route(route).try(:position)
   end
+
+  protected
+
+  def station_route(route)
+  	@station_route ||= routes_stations.where(route: route).first
+  end
+
 end
--- ror/app_test/db/schema.rb
+++ ror/app_test/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160229182706) do
+ActiveRecord::Schema.define(version: 20160302194822) do
 
   create_table "railway_stations", force: :cascade do |t|
     t.string   "name"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20160229182706) do
   create_table "routes_stations", force: :cascade do |t|
     t.integer "railway_station_id"
     t.integer "route_id"
+    t.integer "position"
   end
 
   create_table "tickets", force: :cascade do |t|
