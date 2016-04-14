4220b8463f7cf421bf82aa2c92833d46073272d2
Alexandr <microposts@gmail.com>
2016-04-14 22:37:10 +0300

    Added search

--- ror/app_test/app/controllers/searches_controller.rb
+++ ror/app_test/app/controllers/searches_controller.rb
@@ -1,6 +1,11 @@
 class SearchesController < ApplicationController
 
-  def show  
+  def show
+    if params[:arrive_station] && params[:departure_station]
+      @arrive_station = RailwayStation.find(params[:arrive_station])
+      @departure_station = RailwayStation.find(params[:departure_station])
+      @routes = @arrive_station.routes & @departure_station.routes
+    end  
   end
 
 end
